8f667ada7d55a61b2b35dea2bb172a738d8c755d
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-03-20 22:10:33 +0530

    reset score

--- script.js
+++ script.js
@@ -14,9 +14,30 @@ document.querySelector('.guess').value = 25 */
 // They are a part of web APIs which browsers implement and we can acces them
 // in JS code.
 
+// When we manipulate CSS styles using document.querySelector().style then we are not
+// changing the css files. We are adding inline styles which will affect inside the
+// html tag
+
 let score = 20
 
-const secretNum = Math.trunc(Math.random() * 20 + 1)
+let secretNum = Math.trunc(Math.random() * 20 + 1)
+
+const resetScore = () => {
+  score = 20
+  document.querySelector('.score').textContent = score
+  // generate secretNum
+  secretNum = Math.trunc(Math.random() * 20 + 1)
+  // remove the guessed value
+  document.querySelector('.guess').value = null
+  // black color
+  document.querySelector('body').style.backgroundColor = '#222'
+  // normal width
+  document.querySelector('.number').style.width = '16rem'
+  // remove the secret num
+  document.querySelector('.number').textContent = '?' 
+  // message change
+  document.querySelector('.message').textContent = 'Start guessing...'
+}
 
 document.querySelector('.check').addEventListener('click', () => {
   const guessedNum = Number(document.querySelector('.guess').value)
@@ -24,7 +45,11 @@ document.querySelector('.check').addEventListener('click', () => {
   if (!guessedNum) {
     document.querySelector('.message').textContent = '⛔️ No number entered!'
   } else if (guessedNum === secretNum) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!'  
+    // setTimeout(resetScore, 5000)
+    document.querySelector('.message').textContent = '🎉 Correct Number!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '32rem' 
+    document.querySelector('.number').textContent = guessedNum.toString()
   } else if (guessedNum > secretNum) {
     score = Math.max(score - 1, 0)
     document.querySelector('.message').textContent = (score === 0) 
@@ -37,4 +62,9 @@ document.querySelector('.check').addEventListener('click', () => {
     '📉 Too Low!'
   }
   document.querySelector('.score').textContent = score
+})
+
+document.querySelector('.again').addEventListener('click', () => {
+  // show 20 score
+  resetScore()
 })
